607d107e0dd06de1aeaf04d2f54022a307f54020
etainclub <etainclub@gmail.com>
2020-11-11 08:12:26 +0900

    voting with posting key has error
    - posting key is missing... why it is valid

--- src/providers/blurt/dblurtApi.ts
+++ src/providers/blurt/dblurtApi.ts
@@ -34,7 +34,7 @@ const MAINNET_OFFICIAL = [
   'https://blurtd.privex.io',
   'https://rpc.blurt.buzz',
 ];
-const client = new Client('MAINNET_OFFICIAL', {
+const client = new Client(MAINNET_OFFICIAL, {
   timeout: 5000,
   addressPrefix: 'BLT',
   chainId: 'cd8d90f29ae273abec3eaa7731e25934c63eb654d55080caff2ebb7f5df6381f',
